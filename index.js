1666f71fbf25cf2e8830ddbf399d2d959a51ae89
Juliet Welcome <juliet.welcome@gmail.com>
2018-04-26 03:42:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ function logShout(string) {
   return console.log(string.toUpperCase());
 }
 
+function logWhisper(string) {
+  return console.log(string.toLowerCase());
+}
+
 function sayHiToGrandma(string) {
   if (string.toUpperCase() === string) return "YES INDEED!";
   else if (string === "I love you, Grandma.") return "I love you, too.";
